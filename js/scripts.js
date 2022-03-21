6861105cb17efb92d71c934ec812a2c6d820b3fd
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-21 13:29:43 -0700

    change js code to alert the answer and add js script to html

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,5 @@ function add(num1, num2) {
 
 const num1 = parseInt(prompt("Enter a number:"));
 const num2 = parseInt(prompt("Enter another number:"));
-
-alert(add(num1, num2));
+const result = add(num1, num2);
+alert(result);
