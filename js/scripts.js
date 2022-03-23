aa8e5b065bddeb8450deef13a9bbe08b3d83f78c
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-23 13:48:25 -0700

    add two lines for addition and a button to submit with all scripts working.
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
-function add(num1, num2) {
-	return num1 + num2;
+function add(number1, number2) {
+	return number1 + number2;
 }
 
-function subtract(num1, num2) {
-	return num1 - num2;
+function subtract(number1, number2) {
+	return number1 - number2;
 }
 
-function multiply(num1, num2) {
-	return num1 * num2;
+function multiply(number1, number2) {
+	return number1 * number2;
 }
 
-function divide(num1, num2) {
-	return num1 / num2;
+function divide(number1, number2) {
+	return number1 / number2;
 }
 
-const num1 = parseInt(prompt("Enter a number:"));
-const num2 = parseInt(prompt("Enter another number:"));
-const result = add(num1, num2);
-alert(result);
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+	event.preventDefault();
+	const number1 = parseInt($("#add1").val());
+	const number2 = parseInt($("#add2").val());
+	const result = add(number1, number2);
+	$("#output").text(result);
+	});
+});
