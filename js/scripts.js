514fc4f13d23fc4d9d442687f8dc01c125b58bd3
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-03-24 15:47:42 -0700

    update scripts to a single function for all operators.
    Co-authored-by: Tony Martinez <tony.martinez5897@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,37 +15,21 @@ function divide(number1, number2) {
 }
 
 $(document).ready(function() {
-
-	$("form#add").submit(function(event) {
-	event.preventDefault();
-	const number1 = parseInt($("#add1").val());
-	const number2 = parseInt($("#add2").val());
-	const result = add(number1, number2);
-	$("#addOutput").text(result);
-	});
-
-	$("form#subtract").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#sub1").val());
-		const number2 = parseInt($("#sub2").val());
-		const result = subtract(number1, number2);
-		$("#subOutput").text(result);
-	});
-
-	$("form#multiply").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#multiply1").val());
-		const number2 = parseInt($("#multiply2").val());
-		const result = multiply(number1, number2);
-		$("#multiplyOutput").text(result);
-	});
-
-	$("form#division").submit(function(event) {
-		event.preventDefault();
-		const number1 = parseInt($("#divide1").val());
-		const number2 = parseInt($("#divide2").val());
-		const result = divide(number1, number2);
-		$("#divisionOutput").text(result);
-	});
-
+	$("form#calculator").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
 });
